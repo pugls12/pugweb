dfb3b57ba1dd61aadb085120c9db6662c047cb08
pugls12 <120046993+pugls12@users.noreply.github.com>
2024-03-06 22:20:34 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://dev--rococo-duckanoo-093809.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODI3MjQ1ODY0YmVkMGZiNjMxYTg5NjIyMDI2NjU2OSIsInN1YiI6IjY1YzI2ZjFjYmJlMWRkMDE0NTJhN2FiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hfCYyEo229_ehSL1c3_gEyKj4GXNa5bNWwEA2L22azg",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -12,7 +11,7 @@ window.__CONFIG__ = {
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: false,
 
-  // The backend URL to communicate with
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
   VITE_BACKEND_URL: null,
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
